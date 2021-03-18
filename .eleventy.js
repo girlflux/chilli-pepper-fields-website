ea6b603179bc61bf5ef9667ab1615fd96cc57250
girlflux <19334994+girlflux@users.noreply.github.com>
2021-03-18 17:50:42 +0000

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,12 @@
+module.exports = function(eleventyConfig) {
+
+    eleventyConfig.setTemplateFormats(["md", "njk", "css", "jpg", "svg", "png"]);
+    eleventyConfig.addPassthroughCopy("src/css");
+
+    return {
+        dir: {
+            input: "_src",
+            output: "_public",
+        },
+    };
+};
