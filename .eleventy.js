942e8f47568ddd70d28b10e8c52d6d0283df9bab
girlflux <19334994+girlflux@users.noreply.github.com>
2021-04-10 18:44:27 +0100

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -6,7 +6,7 @@ module.exports = function(eleventyConfig) {
     return {
         dir: {
             input: "_src",
-            output: "dist",
+            output: "_public",
             pathPrefix: "/chilli-pepper-fields-website/"
         },
     };
