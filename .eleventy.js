d43dde2ed71ca01bf1bc7ac45253fcd11983e63b
girlflux <19334994+girlflux@users.noreply.github.com>
2021-04-10 18:40:56 +0100

    changed output folder to 'dist'

--- .eleventy.js
+++ .eleventy.js
@@ -6,7 +6,7 @@ module.exports = function(eleventyConfig) {
     return {
         dir: {
             input: "_src",
-            output: "_public",
+            output: "dist",
             pathPrefix: "/chilli-pepper-fields-website/"
         },
     };
